354d3326e211840cea02d93cba441c089d1b5aeb
admincenec <admincenec@PC104.cenecmalaga.net>
2017-02-02 10:12:09 +0100

    Terminado ejercicio11

--- src/Ejercicio11/Ejercicio11.java
+++ src/Ejercicio11/Ejercicio11.java
@@ -4,17 +4,30 @@ public class Ejercicio11 {
 
 	public static void main(String[] args) {
 
-		String textoBase = "Esto es una prueba de texto, es prueba, es es es, es una prueba";
-		String textoBuscar = "es";
-		
+		String textoBase = "Esto es especificacion una prueba especificacionespecificacion eseses de texto, es es una prueba";
+		String textoBuscar = "especificacion";
+
 		System.out.println(MarcaSubCadena(textoBase, textoBuscar));
 	}
 
-	public static String MarcaSubCadena (String textoBase, String textoBuscar) {
-		String textoDevolver = "";
-		
-		
-		
-		return textoDevolver;
+	public static String MarcaSubCadena(String textoBase, String textoBuscar) {
+		textoBase = textoBase.toLowerCase();
+		textoBuscar = textoBuscar.toLowerCase();
+		int contador = 0;
+
+		do {
+			if (textoBase.indexOf(textoBuscar) == -1)
+				contador++;
+			else {
+				char[] aux = textoBase.toCharArray();
+				int posicionActual = textoBase.indexOf(textoBuscar);
+				aux[posicionActual] = '*';
+				textoBase = String.valueOf(aux);
+				contador = posicionActual + textoBuscar.length();
+			}
+
+		} while (contador < textoBase.length());
+
+		return textoBase;
 	}
 }
