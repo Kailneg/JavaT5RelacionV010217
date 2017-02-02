7ddad80ad3ad1d2ddd107ef5a1a409caf11f4885
admincenec <admincenec@PC104.cenecmalaga.net>
2017-02-02 09:53:28 +0100

    Estructura ej11

---
+++ src/Ejercicio11/Ejercicio11.java
@@ -0,0 +1,20 @@
+package Ejercicio11;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+
+		String textoBase = "Esto es una prueba de texto, es prueba, es es es, es una prueba";
+		String textoBuscar = "es";
+		
+		System.out.println(MarcaSubCadena(textoBase, textoBuscar));
+	}
+
+	public static String MarcaSubCadena (String textoBase, String textoBuscar) {
+		String textoDevolver = "";
+		
+		
+		
+		return textoDevolver;
+	}
+}
